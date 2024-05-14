b7ca2593b86941b8ed560daddc8ca3c85d0cff4e
Max Dubrinsky <max@gretel.ai>
2024-05-14 10:04:48 -0400

    initial commit

---
+++ lua/community.lua
@@ -0,0 +1,15 @@
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.proto" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.git.diffview-nvim" },
+  { import = "astrocommunity.scrolling.nvim-scrollbar" },
+  { import = "astrocommunity.editing-support.multicursors-nvim" },
+  { import = "astrocommunity.programming-language-support.rest-nvim" },
+}
